fdaec0602a5084673bda4f9bf3d83e59741b9705
helene-meneuvrier <hmeneuvrier@talentsoft.com>
2018-04-04 21:44:07 +0200

    ajout d'un composant searchbar

---
+++ src/components/Searchbar.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <div class="bg-indigo" v-if="isLogged">
+        <p>
+            Profil | Annonces | Wishlist | Messagerie
+        </p>
+    </div>
+    <div v-else>
+        Aide | Inscription | Connexion
+    </div>
+
+
+</template>
+
+
+ 
+<script>
+ 
+export default {
+  name:"Navbar",
+  data: function () {
+    return {
+        greeting: 'Hello'
+    }
+  }
+}
+</script>
